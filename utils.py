d4a089607d8972c720546e19feec72a2f7784e38
Emilio Annoni <emilioannoni@live.com>
2022-10-31 10:57:15 +0100

    Added cost tracking methods to utils

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 # AWS imports: Import Braket SDK modules
 from braket.circuits import Circuit, Gate, Instruction, circuit, Observable
 from braket.devices import LocalSimulator
+import braket.tracking.tracker as tracker
 from braket.aws import AwsDevice ,AwsQuantumTask
 import numpy as np
 
@@ -17,37 +18,48 @@ class BraketTaskScanner(object) :
 
     def __init__( self, task_arn = None, qtask = None):
 
+        assert task_arn or qtask
+
         if task_arn :
             qtask = AwsQuantumTask(task_arn)
-        elif not qtask:
-            print("BraketTaskScanner error, not enough information to reconstruct task")
-            return
 
         self._task = qtask
 
         arn = qtask.metadata()["deviceArn"]
-        self._device = AwsDevice(arn)
+        self._dscanner = DeviceScanner(arn)
 
     def get_task(self):
         return self._task
+    
+    def get_arn(self):
+        return self._task._arn
 
     def get_device(self):
-        return self._device    
+        return self._dscanner.get_device()   
+    
+    def get_device_status(self):
+        return self._dscanner.get_status()  
 
     def get_device_arn(self):
-        return self._device.arn
+        return self._dscanner.get_arn()
 
     def get_device_name(self):
-        return self._device.name
+        return self._dscanner.get_name()
 
-    def get_task_status(self):
+    def get_status(self):
         return self.get_task().metadata()["status"] 
+    
+    def get_shots(self):
+        return self.get_task().metadata()["shots"]
 
     def get_device_provider(self):
-        return self.get_device().provider_name
+        return self._dscanner.get_provider()
 
     def get_device_native_gates(self):
-        return self.get_device().properties.paradigm.nativeGateSet
+        return self._dscanner.get_native_gates()
+
+    def get_device_supported_gates(self):
+        return self._dscanner.get_supported_gates()
 
     def get_task_type(self):
         provider = self.get_device_provider()
@@ -61,7 +73,7 @@ class BraketTaskScanner(object) :
         elif provider == "Oxford":
             return "OQC"
 
-    def get_task_results(self):
+    def get_results(self):
         if not self.results_available():
             print("TASK RESULT ERROR: Results not available")
             return 0
@@ -69,13 +81,13 @@ class BraketTaskScanner(object) :
         return self.get_task().result()
 
     def get_circuit(self):
-        result = self.get_task_results()
+        result = self.get_results()
 
         if result :
             return result.additional_metadata.action.source
 
     def get_compiled_circuit(self):
-        result = self.get_task_results()
+        result = self.get_results()
 
         if result :
             type = self.get_task_type()
@@ -93,13 +105,76 @@ class BraketTaskScanner(object) :
             
 
     def task_done(self):
-        return self.get_task_status() in AwsQuantumTask.TERMINAL_STATES
+        return self.get_status() in AwsQuantumTask.TERMINAL_STATES
 
     def results_available(self):
-        return self.get_task_status() in AwsQuantumTask.RESULTS_READY_STATES
+        return self.get_status() in AwsQuantumTask.RESULTS_READY_STATES
 
-    #core device information: arn, provider info, execution window and location
-    def list_device_properties( self):
+    #basic non computational datas about the task
+    def list_task_metadata( self):
+        meta = self.get_task().metadata()
+
+        print("############################")
+        print("Quantum Task Metadata")
+        print("Task arn:", meta["quantumTaskArn"])
+        print("Task shots#:", meta["shots"])
+        print("Task status:", meta["status"])
+        print("Task tags:", meta["tags"])
+        print("Task on device:", meta["deviceArn"])
+        print("Creation date:", meta["createdAt"])
+        print("End date:", meta["endedAt"])
+        print("Output buket data:", meta["outputS3Directory"], " / ", meta["outputS3Bucket"])
+
+    def cost_extimate(self):
+
+        #simulating auto generated task details to reuse traker cost extimate
+        #see implementation of _get_qpu_task_cost for more infos
+        new_details = dict()
+        new_details["status"] = self.get_status()
+        new_details["device"] = self.get_device_arn()
+        new_details["job_task"] = False
+        new_details["shots"] = self.get_shots()
+
+        out = tracker._get_qpu_task_cost( self.get_arn(), new_details)
+        return out
+
+
+
+
+class DeviceScanner(object):
+
+    def __init__( self, device_arn = None, device = None):
+
+        assert device_arn or device
+
+        if device_arn :
+            device = AwsDevice(device_arn)
+
+        self._device = device
+
+    def get_device(self):
+        return self._device    
+
+    def get_status(self):
+        return self.get_device().status
+
+    def get_arn(self):
+        return self.get_device().arn
+
+    def get_name(self):
+        return self.get_device().name
+
+    def get_provider(self):
+        return self.get_device().provider_name
+
+    def get_native_gates(self):
+        return self.get_device().properties.paradigm.nativeGateSet
+
+    def get_supported_gates(self):
+        return self.get_device().properties.action['braket.ir.jaqcd.program'].supportedOperations
+
+     #core device information: arn, provider info, execution window and location
+    def list_properties( self):
 
         device = self.get_device()
         print("#############################")
@@ -117,21 +192,6 @@ class BraketTaskScanner(object) :
             print("No associated device topology")
         else:
             nx.draw_kamada_kawai(device.topology_graph, with_labels=True, font_weight="bold")
-
-    #basic non computational datas about the task
-    def list_task_metadata( self):
-        meta = self.get_task().metadata()
-
-        print("############################")
-        print("Quantum Task Metadata")
-        print("Task arn:", meta["quantumTaskArn"])
-        print("Task shots#:", meta["shots"])
-        print("Task status:", meta["status"])
-        print("Task tags:", meta["tags"])
-        print("Task on device:", meta["deviceArn"])
-        print("Creation date:", meta["createdAt"])
-        print("End date:", meta["endedAt"])
-        print("Output buket data:", meta["outputS3Directory"], " / ", meta["outputS3Bucket"])
 
 
 class Plotter(object) :
